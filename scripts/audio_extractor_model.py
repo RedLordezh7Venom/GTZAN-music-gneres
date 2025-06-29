96129ac24b2aaf87aa52499e3c45bfa4367528b2
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-06-30 03:07:24 +0530

    Add audio extractor model script

---
+++ scripts/audio_extractor_model.py
@@ -0,0 +1,14 @@
+import joblib
+import numpy as np
+from scripts.utilities import predict_audio_class,convert_mp3_to_wav
+from tensorflow.keras.models import load_model 
+
+model = load_model('models/audio_extractor_model.keras')
+converter = joblib.load('models/label_encoder.joblib')
+
+music_file = input("Enter path to music file")
+convert_mp3_to_wav(music_file,'temp/music.wav')
+predaud = predict_audio_class(model, 'temp/music.wav')
+
+predicted_label = converter.inverse_transform([np.argmax(predaud)])
+print("Predicted Genre : ", predicted_label)
